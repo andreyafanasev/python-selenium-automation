d89ec9697ee43bf9bf09361c9f4350246f094ebe
andreyafanasev <afanasyev6@gmail.com>
2020-06-25 17:53:35 -0400

    some changes

--- features/steps/Signed_out_clients_orders.py
+++ features/steps/Signed_out_clients_orders.py
@@ -1,18 +1,19 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from behave import given, when, then
 
 ORDERS_ICON = (By.ID, 'nav-orders')
 SIGN_IN_TEXT = (By.XPATH, "//h1[@class='a-spacing-small']")
 
 
-@When('Click on Orders icon')
+@when('Click on Orders icon')
 def click_orders_icon(context):
     context.driver.find_element(*ORDERS_ICON).click()
     sleep(4)
 
-@Then('Logged out user sees Sign in page')
+
+@then('Logged out user sees Sign in page')
 def verify_found_results_text(context):
-    Sign_in_header = context.driver.find_element(*SIGN_IN_TEXT).text
-    assert 'Sign-In' in Sign_in_header, f'Incorrect header {Sign_in_header}'
+    sign_in_header = context.driver.find_element(*SIGN_IN_TEXT).text
+    assert 'Sign-In' in sign_in_header, f'Incorrect header {sign_in_header}'
