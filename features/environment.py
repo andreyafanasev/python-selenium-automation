7595d2614336299c1fa3fdccfaf9a9fb8fa40916
andreyafanasev <afanasyev6@gmail.com>
2020-07-08 21:50:59 -0400

    some changes

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+from selenium.webdriver.support.wait import WebDriverWait
+from app.application import Application
 
 def browser_init(context):
     """
@@ -11,6 +12,9 @@ def browser_init(context):
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
+    context.app = Application(context.driver)
+
+    context.driver.wait = WebDriverWait(context.driver, 15)
 
 
 def before_scenario(context, scenario):
--- features/steps/serch_result_number.py
+++ features/steps/serch_result_number.py
@@ -4,11 +4,7 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 
 
-@then ("The number of items is equal to {number}")
-def count_items(contex, number):
-    # one_element = contex.driver.find_element(By.CSS_SELECTOR, "div[data-component-type='s-search-result']")
-    # print(one_element)
-    search_result = contex.driver.find_elements(By.CSS_SELECTOR, "div[data-component-type='s-search-result']")
+@then("The number of items is equal to {number}")
+def count_items(context, number):
+    search_result = context.driver.find_elements(By.CSS_SELECTOR, "div[data-component-type='s-search-result']")
     assert len(search_result) == int(number), f"Expected {number}, but got {len(search_result)}"
-    # print(search_result)
-    # print(len(search_result))
